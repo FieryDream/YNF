8d66c032a7ebc9e4166cc385f3b4e2d68cc1e9ad
lobiner <326377007@qq.com>
2016-12-08 16:51:38 +0800

    高军第三次提交，登陆界面完善，注册界面

--- app/src/main/java/com/bw/ynf/views/activity/ZhuYeActivity.java
+++ app/src/main/java/com/bw/ynf/views/activity/ZhuYeActivity.java
@@ -1,17 +1,20 @@
 package com.bw.ynf.views.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.bw.ynf.R;
+import com.bw.ynf.utils.circleimageview.application.MyApp;
 import com.bw.ynf.views.fragment.UserFragment;
 
 import java.util.ArrayList;
@@ -26,6 +29,7 @@ public class ZhuYeActivity extends FragmentActivity implements View.OnClickListe
     private FrameLayout zhuFrame;
     private List<TextView> list;
     private FragmentManager fragmentManager;
+    private MyApp app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,38 +70,57 @@ public class ZhuYeActivity extends FragmentActivity implements View.OnClickListe
         fragmentManager = getSupportFragmentManager();
 
 
-
     }
 
-    //点击事件集合
-    @Override
-    public void onClick(View view) {
-        //每点击一次就把所有的图片和字体颜色还原为默认状态
+    //每点击一次就把所有的图片和字体颜色还原为默认状态
+    public void restartColor(){
+
         for (int i = 0; i <list.size() ; i++) {
             list.get(i).setSelected(false);
             list.get(i).setTextColor(Color.GRAY);
 
         }
+    }
+    //点击事件集合
+    @Override
+    public void onClick(View view) {
+
         switch (view.getId()){
             //点击选中首页
             case R.id.zhu_tv_home:
+                //把所有的图片和字体颜色还原为默认状态
+                restartColor();
                 list.get(0).setSelected(true);
                 list.get(0).setTextColor(Color.RED);
                 break;
             //点击选中分类
             case R.id.zhu_tv_classify:
+                //把所有的图片和字体颜色还原为默认状态
+                restartColor();
                 list.get(1).setSelected(true);
                 list.get(1).setTextColor(Color.RED);
                 break;
             //点击选中购物车
             case R.id.zhu_tv_shopp:
-                list.get(2).setSelected(true);
-                list.get(2).setTextColor(Color.RED);
-                Intent intent=new Intent(ZhuYeActivity.this,LoGinActivity.class);
-                startActivity(intent);
+                SharedPreferences sp = MyApp.getShared();
+//                sp.getString("succese","loginSuccese");
+                String loginInfo = sp.getString("succese", "");
+                if(loginInfo.equals("loginSuccese")){
+                    //把所有的图片和字体颜色还原为默认状态
+                    restartColor();
+                    list.get(2).setSelected(true);
+                    list.get(2).setTextColor(Color.RED);
+                }else{
+                    Intent intent=new Intent(ZhuYeActivity.this,LoGinActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.huanying_enter1,R.anim.huanying_exit1);
+                }
+
                 break;
             //点击选中我的
             case R.id.zhu_tv_user:
+                //把所有的图片和字体颜色还原为默认状态    
+                restartColor();
                 list.get(3).setSelected(true);
                 list.get(3).setTextColor(Color.RED);
                 fragmentManager.beginTransaction().replace(R.id.frame_content,new UserFragment()).commit();
